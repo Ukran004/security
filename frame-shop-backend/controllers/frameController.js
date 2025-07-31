3cd4ee7753c23f348b9408d73931dd02b6b56a69
Ukran004 <ukrantandukar13@gmail.com>
2025-07-31 15:32:06 +0545

    Changes.

--- frame-shop-backend/controllers/frameController.js
+++ frame-shop-backend/controllers/frameController.js
@@ -41,7 +41,7 @@ exports.getAllHelmets = async (req, res) => {
       ];
     }
 
-    // Sortin
+    // Sorti
     const sortOptions = {};
     sortOptions[sortBy] = sortOrder === 'desc' ? -1 : 1;
 
