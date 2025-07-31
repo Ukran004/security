e34303b0ff9570a2e672edab963196767243f409
Ukran004 <ukrantandukar13@gmail.com>
2025-07-31 15:33:45 +0545

    Changes.

--- frame-shop-backend/controllers/frameController.js
+++ frame-shop-backend/controllers/frameController.js
@@ -69,7 +69,7 @@ exports.getAllHelmets = async (req, res) => {
   }
 };
 
-// Get single helm
+// Get single hel
 exports.getHelmetById = async (req, res) => {
   try {
     const helmet = await Helmet.findById(req.params.id);
