3020f61b754e658b95c8baec01571887e4fad05a
Ukran004 <ukrantandukar13@gmail.com>
2025-07-31 15:28:10 +0545

    changes

--- frame-shop-backend/controllers/frameController.js
+++ frame-shop-backend/controllers/frameController.js
@@ -32,7 +32,7 @@ exports.getAllHelmets = async (req, res) => {
       if (maxPrice) filter.price.$lte = parseFloat(maxPrice);
     }
 
-    // Search functionality
+    // Search functionalit
     if (search) {
       filter.$or = [
         { name: { $regex: search, $options: 'i' } },
