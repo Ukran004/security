170c03005b7073b7098e6936fbd658e8c7db1a44
Ukran004 <ukrantandukar13@gmail.com>
2025-07-31 15:26:50 +0545

    changes

--- frame-shop-backend/controllers/frameController.js
+++ frame-shop-backend/controllers/frameController.js
@@ -1,7 +1,7 @@
 const Helmet = require("../models/Helmet");
 const logActivity = require('../middleware/activityLogger');
 
-// Get al
+// Get a
 exports.getAllHelmets = async (req, res) => {
   try {
     const {
